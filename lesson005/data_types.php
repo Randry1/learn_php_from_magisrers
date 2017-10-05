f90618ec0a01e63d9366bf805b7dcd47acaea21b
Randry1 <revinandry@gmail.com>
2017-10-05 10:29:07 +0700

    data_type

---
+++ lesson005/data_types.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+
+//*********************
+// ТИПЫ ДАННЫЙХ В PHP
+// string
+// integer
+// float (double)
+// boolean
+// array
+// object
+// null
+// resource
+
+$string = 'Hello World!';
+$integer = -13;
+$boolean = true;
+$array = array('sdf','dsf ',2,$string);// одномерные, ассоцеативные, многомерные
+echo $array[1 - 1]; // => sdf
+$arrayTwo = array('sdf','dsf ',2,$string);// ассоцеативные, многомерные
+
+
